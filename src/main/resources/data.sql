53f85b685877e7c1ef622bcb9464668fed996fdf
Alexander Berg <alexander.berg.job@gmail.com>
2023-02-12 14:36:37 +0100

    added docker compose for mysql server and did minor changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,13 @@ INSERT INTO role VALUES (2, 'Super User');
 INSERT INTO role VALUES (3, 'User');
 INSERT INTO role VALUES (4, 'Visitor');
 
-INSERT INTO category VALUES (1, 'Electronics');
-INSERT INTO category VALUES (2, 'Automobile');
-INSERT INTO category VALUES (3, 'Sport');
-INSERT INTO category VALUES (4, 'Food and Beverage');
+INSERT INTO brand VALUES (1, 'Dell');
+INSERT INTO brand VALUES (2, 'BMW');
+INSERT INTO brand VALUES (3, 'Adiddas');
+INSERT INTO brand VALUES (4, 'Nestle');
+
+
+INSERT INTO category VALUES (1, 'Electronics', 1);
+INSERT INTO category VALUES (2, 'Automobile', 2);
+INSERT INTO category VALUES (3, 'Sport', 3);
+INSERT INTO category VALUES (4, 'Food and Beverage', 4);
